ebccd39651dbf9cd5bd0bc8fb4d7c350cde84979
krodiv <krodiv@gmail.com>
2023-07-17 10:45:22 -0400

    added beautifulsoup and made simple implementation

--- estate_scraper/inital_scraper.py
+++ estate_scraper/inital_scraper.py
@@ -1,12 +1,16 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import requests
+from bs4 import BeautifulSoup
 
 class Scraper:
     def __init__(self):
         self.driver = None
+        self.soup = None
 
     def create_driver(self, driver_type=None):
+        #For selenium use
         if driver_type:
             pass
             #set given driver type
@@ -16,10 +20,24 @@ class Scraper:
         return 
 
     def get_source(self, source):
-        #do something here for setting up a source
+        #get website via selenium
         self.driver.get(source)
         return self.driver
 
     def find_element_by_class_name(self, element_name):
+        #get website element with selenium
         content = self.driver.find_element(By.CLASS_NAME, element_name)
         return content
+
+    def get_website_text(self, url):
+        #get website content via requests
+        return requests.get(url).text
+
+    def init_bs4(self, url):
+        #initalize beautiful soup object
+        text = self.get_website_text(url)
+        self.soup = BeautifulSoup(text, "html.parser")
+
+    def find_sb4(self, tag_type, class_name):
+        tag_list = self.soup.find_all(tag_type, class_name)
+        return tag_list
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,14 +1,24 @@
 import estate_scraper.inital_scraper as scrape
 
-def test_scraper_creation():
+def test_selenium_creation():
     scraper = scrape.Scraper()
     scraper.create_driver()
     
     res = scraper.get_source("https://www.realtor.ca/on/toronto/real-estate")
     assert "Toronto" in res.title
 
-    content = scraper.find_element_by_class_name("listingCardPrice")
-    print(content.get_attribute("title"))
+    # content = scraper.find_element_by_class_name("listingCardPrice")
+    # print(content.get_attribute("title"))
+    #
+    # assert 1 == 0
+
+def test_soup_creation():
+    url = "http://toronto.listing.ca/"
+    scraper = scrape.Scraper()
+    scraper.init_bs4(url)
+
+    result = scraper.find_sb4("div", {"class": "sl"})
+    print(len(result))
+    print(result[0].contents)
 
     assert 1 == 0
-
